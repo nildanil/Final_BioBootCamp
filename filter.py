ff778bdab43c485a61e62251d4ce6ddc636c11d2
nildanil <64488861+nildanil@users.noreply.github.com>
2020-12-06 21:54:05 +0300

    Файлы с вставками

--- filter.py
+++ filter.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
 
-num_of_rank = float(input("Введите рамку на rank(<): "))
-num_of_score = float(input("Введите рамку на score(>): "))
-file_name = input("Введите имя файла, в котором будет список эпитопов: ")
-with open(file_name, 'w') as f:
-    pass
+num_of_rank = float(input("Введите рамку на rank(>): "))
+num_of_score = float(input("Введите рамку на score(<): "))
+file_name = (input("Введите имя файла, в котором будет список эпитопов: ") +'.fasta')
 
 dir_name = input('Укажите папку с данными: ')
 a = int(input('Введите цифру на первом файле с результатом. Пример: result-15.csv => 15: '))
@@ -14,7 +12,7 @@ b = int(input('Введите цифру на последнем файле: '))
 
 array_of_files = [f'result-{i}.csv' for i in range(a, b+1)]
 
-df = pd.DataFrame()
+list_of_proteins =  list()
 
 for i in array_of_files:
     try:
@@ -22,11 +20,17 @@ for i in array_of_files:
         epitope_data['score'] = pd.to_numeric(epitope_data['score'], errors='coerce', downcast='float'
 )
         epitope_data['rank'] = pd.to_numeric(epitope_data['rank'], errors='coerce', downcast='float')
-        new_data = epitope_data[(epitope_data['rank'] < num_of_rank) & (epitope_data['score'] > num_of_score)]
+        new_data = epitope_data[(epitope_data['rank'] > num_of_rank) & (epitope_data['score'] < num_of_score)] #Эта строка - ключевая, её можно менять под свои нужды
 
-        df = pd.concat([df, new_data])
     except:
         pass
 
+list_of_proteins = new_data['peptide']
 
-df.to_csv(file_name)
+print(*list_of_proteins)
+with open(file_name, 'a') as f: 
+    for i, it in enumerate(list_of_proteins):
+        f.write(f'>{i}\n{it}\n')
+
+        
+    
